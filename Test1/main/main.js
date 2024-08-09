2d6ba78893d9c649214d59b1432c2f79c06a8b0b
ducmanh02 <89408872+ducmanh02@users.noreply.github.com>
2024-08-09 16:08:05 +0700

    update mobile

--- Test1/main/main.js
+++ Test1/main/main.js
@@ -1,10 +1,24 @@
-function toggleHomeModal() {
+const closeAdsNotification = () =>{
+  const AdsNotification = document.querySelector(".notification");
+  AdsNotification.style.display = "none";
+}
+
+
+const toggleHomeModal = () => {
   const modal = document.querySelector(".header-home-hover-modal");
   modal.classList.toggle("show");
+  const homeLinkBtn = document.querySelector(".home-link-btn");
+  homeLinkBtn.classList.toggle("active-header-btn");
+  const homeLinkIcon = document.querySelector(".home-link-icon");
+  homeLinkIcon.classList.toggle("active-header-btn-icon");
 }
-function toggleShopModal() {
+const toggleShopModal = () => {
   const modal = document.querySelector(".header-shop-hover-modal");
   modal.classList.toggle("show");
+  const shopLinkBtn = document.querySelector(".shop-link-btn");
+  shopLinkBtn.classList.toggle("active-header-btn");
+  const shopLinkIcon = document.querySelector(".shop-link-icon");
+  shopLinkIcon.classList.toggle("active-header-btn-icon");
 }
 
 function toggleMinicart() {
@@ -13,8 +27,64 @@ function toggleMinicart() {
   minicart.classList.toggle("show");
   overlay.classList.toggle("show");
 }
+const toggleNavbar= () =>{
+  const nav = document.querySelector(".nav-container");
+  const overlay2 = document.querySelector(".overlay2");
+  nav.classList.toggle("show");
+  overlay2.classList.toggle("show");
+}
+
+const toggleSubHome = () =>{
+  const navSubHome = document.querySelector(".nav-container-submenu-home");
+  const overlay2 = document.querySelector(".overlay2");
+  navSubHome.classList.toggle("show");
+  overlay2.classList.toggle("show");
+}
+
+//freeship remaining
+
+const freeshipRemaingContainer = document.querySelector(
+  ".freeship-remaining-container"
+);
+const freeshipRemainingText = document.querySelector(
+  ".freeship-remaining-text"
+);
+
+const updateLoadingFreeship = (totalPrice) => {
+  const loadingProgress = document.querySelector(".loading-progress");
+  if (totalPrice < 500) {
+    const percent = (totalPrice / 500) * 100;
+    loadingProgress.style.width = `${percent}%`;
+  } else {
+    loadingProgress.style.width = "100%";
+  }
+};
+
+const remainingPriceInnerContent = (totalPrice) => {
+  if (totalPrice < 500) {
+    freeshipRemainingText.textContent = `Spend $${(500 - totalPrice).toFixed(
+      2
+    )} more and get `;
+  } else {
+    freeshipRemainingText.textContent = `Congratulations, you got `;
+  }
+};
 
 let cartItems = [
+  {
+    image: "./assets/images/product/product11.jpg",
+    name: "Square Textured Striped",
+    color: "Black / M",
+    price: 145.0,
+    quantity: 1,
+  },
+  {
+    image: "./assets/images/product/product12.jpg",
+    name: "Floral Print Dress",
+    color: "Blue / S",
+    price: 89.99,
+    quantity: 2,
+  },
   {
     image: "./assets/images/product/product11.jpg",
     name: "Square Textured Striped",
@@ -47,7 +117,7 @@ const createCartItemHTML = (item) => {
                                         height="100%">
                                 </div>
                                 <div class="content d-flex justify-content-between flex-1">
-                                    <div class="d-flex flex-column gap-19">
+                                    <div class="d-flex flex-column gap-10">
                                         <div class="detail">
                                             <p class="fs-16 fw-400 mb-10">
                                                 ${item.name}
@@ -60,11 +130,11 @@ const createCartItemHTML = (item) => {
                                             )}</p>
                                         </div>
                                         <div class="quantity-stepper fs-14 fw-400">
-                                            <button onclick="decreaseQuantity(this)">-</button>
+                                            <button onclick="decreaseQuantity(this)"><span class="icon-minus"></span></button>
                                             <span class="quantity" class="fs-14 fw-400">${
                                               item.quantity
                                             }</span>
-                                            <button onclick="increaseQuantity(this)">+</button>
+                                            <button onclick="increaseQuantity(this)"><span class="icon-plus"></span></button>
                                         </div>
                                     </div>
                                     <div class="action d-flex flex-column gap-10">
@@ -76,14 +146,6 @@ const createCartItemHTML = (item) => {
   `;
   return cartItem;
 };
-const updateLoadingFreeship = (totalPrice) =>{
-  if(totalPrice < 500){
-    return totalPrice/500;
-  }
-  else{
-    return 1;
-  }
-}
 
 const updateTotalPrice = () => {
   const totalPrice = cartItems.reduce(
@@ -91,30 +153,31 @@ const updateTotalPrice = () => {
     0
   );
   totalPriceElement.textContent = `$${totalPrice.toFixed(2)}`;
+  remainingPriceInnerContent(totalPrice);
+  updateLoadingFreeship(totalPrice);
 };
 
 const decreaseQuantity = (button) => {
   const cartItem = button.closest(".item");
   const quantityElement = cartItem.querySelector(".quantity");
   const index = Array.from(cartItemsContainer.children).indexOf(cartItem);
-  // console.log(index);
 
   if (cartItems[index].quantity > 1) {
     cartItems[index].quantity--;
     quantityElement.textContent = cartItems[index].quantity;
     updateTotalPrice();
+    updateTotalQuantity();
   }
 };
 function increaseQuantity(button) {
   const cartItem = button.closest(".item");
   const quantityElement = cartItem.querySelector(".quantity");
   const index = Array.from(cartItemsContainer.children).indexOf(cartItem);
-  // console.log(index);
 
   cartItems[index].quantity++;
   quantityElement.textContent = cartItems[index].quantity;
   updateTotalPrice();
-  console.log("increment quantity");
+  updateTotalQuantity();
 }
 function removeFromCart(button) {
   const cartItem = button.closest(".item");
@@ -122,6 +185,7 @@ function removeFromCart(button) {
   cartItems.splice(index, 1);
   cartItem.remove();
   updateTotalPrice();
+  updateTotalQuantity();
 }
 // generate sản phẩm
 cartItems.forEach((item) => {
@@ -130,3 +194,26 @@ cartItems.forEach((item) => {
 });
 
 updateTotalPrice();
+
+const updateTotalQuantity = () => {
+  const items = document.querySelectorAll(".list-item-cart .item");
+
+  let totalProducts = 0;
+  items.forEach((item) => {
+    const quantity = parseInt(item.querySelector(".quantity").textContent);
+    totalProducts += quantity;
+  });
+  console.log(totalProducts);
+
+  document.querySelector(".icon-notice .total-quantity").textContent =
+    totalProducts;
+  if(totalProducts == 0){
+    const cartItem = document.createElement("div");
+    cartItemsContainer.appendChild(cartItem);
+    console.log("0");
+    cartItem.innerHTML = `<p class="fs-13 fw-400 text-center">No products in cart</p>`;
+  }
+};
+
+updateTotalQuantity();
+
